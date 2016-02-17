dbe7e6a70ca3c1dffb916cc12d2feae488da24e0
Boothand <boothand@hotmail.com>
2016-02-18 00:31:21 +0100

    Basic ocean scroll script/shader

---
+++ GalapagosUnity/Assets/Scripts/OceanScroll.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class OceanScroll : MonoBehaviour
+{
+	Renderer rend;
+	public Vector2 diffuseScroll = Vector2.one;
+	public Vector2 normalScroll = Vector2.one;
+	Vector2 diffScroll;
+	Vector2 nrmScroll;
+	float waveTimer;
+
+	void Start ()
+	{
+		rend = GetComponent<Renderer>();
+		diffScroll = rend.material.GetTextureOffset("_MainTex");
+		nrmScroll = rend.material.GetTextureOffset("_BumpMap");
+	}
+	
+	void Update ()
+	{
+		diffScroll += diffuseScroll * Time.deltaTime;
+		nrmScroll += normalScroll * Time.deltaTime;
+
+		waveTimer += Time.deltaTime;
+
+		rend.material.SetTextureOffset("_MainTex", diffScroll);
+		rend.material.SetTextureOffset("_DetailAlbedoMap", nrmScroll);
+		
+	}
+}
