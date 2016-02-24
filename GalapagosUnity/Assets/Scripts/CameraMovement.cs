31567f70541e4e3bc33b2d5add9782d3cef9509a
Boothand <boothand@hotmail.com>
2016-02-24 15:32:45 +0100

    Initial camera movement script

---
+++ GalapagosUnity/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    Vector3 targetPos;
+
+	void Start ()
+    {
+        targetPos = transform.position;
+	}
+	
+	void Update ()
+    {
+        targetPos += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f) * speed * Time.deltaTime;
+//        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * speed);
+        transform.position = targetPos;
+	}
+}
