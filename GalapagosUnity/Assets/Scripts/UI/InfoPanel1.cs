230584c656ff6bcf4d17a03df6bdd8fa6c03a05d
Boothand <boothand@hotmail.com>
2016-02-26 19:54:37 +0100

    Starter UI with basic info

---
+++ GalapagosUnity/Assets/Scripts/UI/InfoPanel1.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanel1 : MonoBehaviour
+{
+	public Text selectedObjectName;
+	public Text infoField1;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if (GameManager.selectedObject)
+		{
+			selectedObjectName.enabled = true;
+			selectedObjectName.text = GameManager.selectedObject.typeName;
+
+			if (GameManager.selectedObject.GetComponent<Fisherboat>())
+			{
+				infoField1.enabled = true;
+				infoField1.text = GameManager.selectedObject.GetComponent<Fisherboat>().workers + "/" + GameManager.selectedObject.GetComponent<Fisherboat>().workerCapacity;
+			}
+		}
+		else
+		{
+			selectedObjectName.enabled = false;
+			infoField1.enabled = false;
+		}
+	}
+}
