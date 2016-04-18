c12e339d6f4a40f097d2f652776cbe812d68e35e
Zanzellian <storholtkent@gmail.com>
2016-04-18 11:29:52 +0200

    Set up missions. Fix if need be. no time. yaaaay,

---
+++ GalapagosUnity/Assets/Scripts/UI/MissionScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionScript : MonoBehaviour
+{
+	public GameObject missionInfoBar;
+	public GameObject missionPanel;
+
+	public GameObject survivalMission;
+	public GameObject fishermenImpact;
+	public GameObject timeWaits;
+
+	void Start()
+	{
+		missionInfoBar.SetActive(false);
+		missionPanel.SetActive(false);
+	}
+
+	public void CheckMissions()
+	{
+		missionInfoBar.SetActive(true);
+	}
+
+	public void ExitMissionPanel()
+	{
+		missionInfoBar.SetActive(false);
+	}
+
+	public void exitMissionLogPanel()
+	{
+		missionPanel.SetActive(false);
+		survivalMission.SetActive(false);
+		fishermenImpact.SetActive(false);
+		timeWaits.SetActive(false);
+	}
+
+	public void MissionLog1()
+	{
+		missionPanel.SetActive(true);
+		survivalMission.SetActive(true);
+	}
+
+	public void MissionLog2()
+	{
+		missionPanel.SetActive(true);
+		fishermenImpact.SetActive(true);
+	}
+
+	public void MissionLog3()
+	{
+		missionPanel.SetActive(true);
+		timeWaits.SetActive(true);
+	}
+
+	void Update()
+	{
+
+	}
+}
