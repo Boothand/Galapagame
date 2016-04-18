43f30507934fd446305d0efa4641525505160107
Zanzellian <storholtkent@gmail.com>
2016-04-18 11:29:27 +0200

    Made new script for faction. use for money and so on.

---
+++ GalapagosUnity/Assets/Scripts/FactionScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FactionScript : MonoBehaviour {
+
+	public Stats.Faction faction;
+
+	int totalMoney;
+	int totalFish;
+	int totalWorkers;
+
+	int totalBoats;
+	int totalWorkstations;
+
+	// Use this for initialization
+	void Start () 
+	{
+		totalWorkstations = 1;
+		totalBoats = 1;
+		totalWorkers = 5;
+		totalMoney = 1000;
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
