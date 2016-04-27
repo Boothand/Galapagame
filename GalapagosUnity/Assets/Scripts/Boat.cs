82c5f0579ef3ef2e2485bbb707727fbfb30449d7
Zanzellian <storholtkent@gmail.com>
2016-04-27 23:08:26 +0200

    changes in boat, moved to Fisherboat.cs

--- GalapagosUnity/Assets/Scripts/Boat.cs
+++ GalapagosUnity/Assets/Scripts/Boat.cs
@@ -6,26 +6,12 @@ public class Boat : Mover
 	public Stats.Faction faction;
 
 	[SerializeField]
-	FishZone[] fiskesoner = new FishZone[7];
-	[SerializeField]
-	Fisherboat[] boats = new Fisherboat[10];
 	public FactionScript myFaction;
-
-	internal Stats stats;
-	Pathfinder path;
-
-	float ZoneDistanceOld;
-	float ZoneDistanceNew;
-	Transform oldZone;
-	Transform NewZone;
-	Transform useZone;
-
-	int currentZone = 0;
 
 
 	void Start ()
 	{
-		stats = GetComponent<Stats>();
+		BaseStart();
 	}
 
 	internal override void BaseUpdate()
@@ -55,72 +41,7 @@ public class Boat : Mover
 		
 	}
 
-	IEnumerable mainBoatAI()
-	{
-		yield return null;
-	}
-
-	IEnumerable findZone()
-	{
-		//finds a sone
-		goToZone();
-		yield return null;
-	}
-
-	IEnumerable returnToBase()
-	{
-		//starts a routine for returning to base with fish
-		yield return null;
-	}
-
-	void goToZone()
-	{
-		//looping through all fishzones
-		foreach(FishZone soner in fiskesoner)
-		{
-			if (!oldZone)
-			{
-				oldZone = soner.transform;
-				currentZone++;
-			}
-			else
-			{
-				NewZone = soner.transform;
-
-				ZoneDistanceOld = Mathf.Sqrt(Mathf.Pow(oldZone.position.x - this.transform.position.x, 2) + Mathf.Pow(oldZone.position.y - this.transform.position.y, 2));
-				ZoneDistanceNew = Mathf.Sqrt(Mathf.Pow(NewZone.position.x - this.transform.position.x, 2) + Mathf.Pow(NewZone.position.y - this.transform.position.y, 2));
-
-				if (ZoneDistanceOld > ZoneDistanceNew)
-				{
-					useZone = oldZone;
-					currentZone++;
-				}
-				else if (ZoneDistanceNew > ZoneDistanceOld)
-				{
-					oldZone = NewZone;
-					useZone = NewZone;
-					currentZone++;
-				}
-			}
-
-			if (currentZone == fiskesoner.Length)
-			{
-				Vector3 fishZoneTarget = new Vector3(useZone.position.x, useZone.position.y, useZone.position.z);
-				path.GoToPos(fishZoneTarget);
-			}
-		}
-	}
-
-	void checkForEmptyBoats()
-	{
-		// checks for boats that can be sent to a zone.
-		//this one must be done before goToZone()
-	}
-
-	void findBase(Transform basePosition)
-	{
-		//finding the position of your base, in order to return when full of fish.
-	}
+	
 
 
 
